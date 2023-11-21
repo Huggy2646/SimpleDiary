30bec5fafdc34766ae798205a8a4bfb0688d963c
Huggy2646 <osh990129@gmail.com>
2023-11-21 15:45:36 +0900

    DiaryItem and DiaryList update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
 
+const dummyList = [
+    { id: 1, author: '오성혁', content: 'hi_1', emotion: 5, created_date: new Date().getTime() },
+    { id: 2, author: '홍길동', content: 'hi_2', emotion: 3, created_date: new Date().getTime() },
+    { id: 3, author: '이영희', content: 'hi_3', emotion: 2, created_date: new Date().getTime() },
+    { id: 4, author: '이철수', content: 'hi_4', emotion: 1, created_date: new Date().getTime() },
+];
 function App() {
     return (
         <div className="App">
             <DiaryEditor />
+            <DiaryList diaryList={dummyList} />
         </div>
     );
 }
